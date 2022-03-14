01796d82e32e12f66a4e436e0cfa874b8e00c0c4
DangPhuc141101 <dangphucff@gmail.com>
2022-03-14 14:43:19 +0700

    add style sheet

--- routers/accounts.js
+++ routers/accounts.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const accountController = require('../controllers/accounts');
 
-router.post('/', accountController.createAccount)
-
+router.get('/register', accountController.registerForm)
+router.post('/register', accountController.createAccount)
+router.get('/login', accountController.loginForm)
+router.post('/login', accountController.login)
   
 module.exports = router;
