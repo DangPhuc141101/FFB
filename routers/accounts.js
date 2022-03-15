fc9dff4315844e46800d09a218826a6280cd98cc
DangPhuc141101 <dangphucff@gmail.com>
2022-03-15 17:00:10 +0700

    add footer and navbar

--- routers/accounts.js
+++ routers/accounts.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const accountController = require('../controllers/accounts');
 
-router.get('/register', accountController.registerForm)
-router.post('/register', accountController.createAccount)
-router.get('/login', accountController.loginForm)
-router.post('/login', accountController.login)
+// router.get('/register', accountController.registerForm)
+// router.post('/register', accountController.createAccount)
+// router.get('/login', accountController.loginForm)
+// router.post('/login', accountController.login)
   
 module.exports = router;
