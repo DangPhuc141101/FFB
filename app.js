cc158f5d1d8e6fbc639bf599c72f6fde495edd15
DangPhuc141101 <dangphucff@gmail.com>
2022-03-20 22:57:16 +0700

    add map, flash, hadle login, logout, register

--- app.js
+++ app.js
@@ -5,9 +5,22 @@ if (process.env.NODE_ENV !== "production"){
 const express = require('express');
 const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const session = require('express-session');
+const flash = require('connect-flash');
+const Account = require('./models/accounts');
 const path = require('path');
 const app = express();
 
+// Create server
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+
+const io = new Server(server);
+
+const ExpressError = require('./utils/ExpressError');
 const connectDB = require('./configs/dbConfig');
 connectDB();
 
@@ -18,13 +31,56 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static('public'));
 app.use(methodOverride('_method'));
 
+const sessionConfig = {
+    secret: 'Thisisscret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,  // hết hạn sau 7 ngày.
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+
+app.use(flash());
+
+app.use(session(sessionConfig));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(Account.authenticate()));
+
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
+
+app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
+
 const fieldRouter = require('./routers/fields');
 const accountRouter = require('./routers/accounts');
 
 app.use('/', fieldRouter);
 app.use('/accounts', accountRouter);
 
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page not found', 404));
+});
+
+app.use((err, req, res, next) => {
+    const {statusCode = 500, message = 'Something went wrong'} = err;
+    if (!err.message) error.message = 'Oh no, Something went wrong!';
+    res.status(statusCode).render('error', {err});
+});
+
+io.on('connection', function(socket) {
+    console.log('user connected');
+})
+
 const PORT = process.env.PORT || 333; 
-app.listen(333, ()=>{
+server.listen(333, ()=>{
     console.log("Severs listening on PORT ", PORT);
 })
--- controllers/accounts.js
+++ controllers/accounts.js
@@ -2,11 +2,27 @@ const Account = require('../models/accounts');
 
 // Register account
 module.exports.createAccount = async(req, res) => {
+   try {
     const { email, username, password } = req.body;
-    const user = new User({ email, username });
-    const registerUser = await User.register(user, password);
-
-    // if (registerUser) {
-    //     res.se
-    // }
+    console.log(req.body)
+    const account = new Account({ email, username });
+    const registerAccount = await Account.register(account, password);
+    console.log("Register success!")
+    req.login(registerAccount, err =>{
+        if (err) return next(err);
+        res.redirect('/');
+    })
+   }
+   catch(e) {
+       console.log(e);
+   }
 }
+
+module.exports.login = async(req, res) => {
+    res.redirect('/');
+}
+
+module.exports.logout = (req, res) => {
+    req.logout();
+}
+
